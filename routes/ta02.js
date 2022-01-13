7643073b5dc216cf9aaf8d648aad5b64aabc7881
Griffin Pope <grriffin@gmail.com>
2022-01-12 20:36:11 -0700

    Finished team activity 2

--- routes/ta02.js
+++ routes/ta02.js
@@ -3,13 +3,52 @@
 const express = require('express');
 const router = express.Router();
 
+const users = [
+  {
+    name: 'Homer Simpson',
+  },
+  {
+    name: 'Bart Simpson',
+  },
+  {
+    name: 'Marge Simpson',
+  },
+];
+
+let error = '';
+
 router.get('/', (req, res, next) => {
-  res.render('pages', {
+  res.render('pages/ta02', {
     title: 'Team Activity 02',
     path: '/ta02', // For pug, EJS
     activeTA03: true, // For HBS
     contentCSS: true, // For HBS
+    users: users,
+    error: error,
   });
+  error = '';
+});
+
+router.post('/addUser', (req, res, next) => {
+  const name = req.body.name;
+  const index = users.findIndex((n) => n.name === name);
+  if (index >= 0) {
+    error = 'User already exists!';
+  } else {
+    users.push({ name });
+  }
+  res.redirect('/ta02');
+});
+
+router.post('/removeUser', (req, res, next) => {
+  const name = req.body.name;
+  const index = users.findIndex((n) => n.name === name);
+  if (index >= 0) {
+    users.splice(index, 1);
+  } else {
+    error = 'User does not exist!';
+  }
+  res.redirect('/ta02');
 });
 
 module.exports = router;
