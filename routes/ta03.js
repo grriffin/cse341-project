62030fb2cd8dd67185bc0b50ebf5a54368d3b402
Griffin Pope <grriffin@gmail.com>
2022-01-19 20:59:44 -0700

    Updated with team activity 3

--- routes/ta03.js
+++ routes/ta03.js
@@ -2,12 +2,31 @@
 const express = require('express');
 const router = express.Router();
 
+const fs = require('fs');
+const path = require('path');
+
 router.get('/', (req, res, next) => {
-  res.render('pages/ta03', {
-    title: 'Team Activity 03',
-    path: '/ta03', // For pug, EJS
-    activeTA03: true, // For HBS
-    contentCSS: true, // For HBS
+  const p = path.join(__dirname, '..', 'data', 'products.json');
+  fs.readFile(p, (err, content) => {
+    if (err) {
+      console.log(err);
+    }
+    let products = JSON.parse(content);
+    const criteria = req.query.criteria;
+    if (criteria) {
+      products = products.filter(
+        (product) =>
+          product.name.toLowerCase().search(criteria.toLowerCase()) >= 0
+      );
+    }
+
+    res.render('pages/ta03', {
+      title: 'Team Activity 03',
+      path: '/ta03', // For pug, EJS
+      products: products,
+      activeTA03: true, // For HBS
+      contentCSS: true, // For HBS
+    });
   });
 });
 
